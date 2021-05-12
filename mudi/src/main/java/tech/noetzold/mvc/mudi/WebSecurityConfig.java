8be6140ff2f1f6066ab9dff8c5760dcf9445e5b6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-05-12 17:23:37 -0300

    cache

--- mudi/src/main/java/tech/noetzold/mvc/mudi/WebSecurityConfig.java
+++ mudi/src/main/java/tech/noetzold/mvc/mudi/WebSecurityConfig.java
@@ -26,26 +26,36 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.anyRequest().authenticated()
+		.antMatchers("/home/**")
+			.permitAll()
+		.anyRequest()
+			.authenticated()
 		.and()
 		.formLogin(form -> form
             .loginPage("/login")
-            .defaultSuccessUrl("/home", true)
+            .defaultSuccessUrl("/usuario/pedido", true)
             .permitAll()
         )
-		.logout(logout -> logout.logoutUrl("/logout"))
-		.csrf().disable();
+		.logout(logout -> {
+			logout.logoutUrl("/logout")
+				.logoutSuccessUrl("/home");
+		});
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
 		auth
 			.jdbcAuthentication()
 			.dataSource(dataSource)
 			.passwordEncoder(encoder);
-
+		
+//		UserDetails user =
+//				 User.builder()
+//					.username("maria")
+//					.password(encoder.encode("maria"))
+//					.roles("ADM")
+//					.build();
 	}
 	
 }
