284f01b4792c5d21f167838cd13660b136081364
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-05-13 16:06:24 -0300

    vue.js

--- mudi/src/main/java/tech/noetzold/mvc/mudi/controller/OfertaController.java
+++ mudi/src/main/java/tech/noetzold/mvc/mudi/controller/OfertaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/oferta")
 public class OfertaController {
 
+	
 	@GetMapping
 	public String getFormularioParaOfertas() {
 		return "oferta/home";
